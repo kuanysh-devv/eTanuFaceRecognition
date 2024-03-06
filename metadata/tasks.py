07206256ed68f7916d3f9618c2da45fcb5dae3d2
Kuanysh <kuanysh01012@gmail.com>
2024-03-06 09:08:29 +0500

    Adding celery multiproccessing

--- metadata/tasks.py
+++ metadata/tasks.py
@@ -1,17 +1,15 @@
+from celery import shared_task
+import cv2
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from django.http import JsonResponse, HttpResponse
 import time
-from .tasks import process_image
-import cv2
 import torch
 import os
 import uuid
 import sys
 import numpy as np
-from dotenv import load_dotenv
-
 import torchvision.transforms as transforms
 import torchvision.models as models
 from torchvision.models import ResNet50_Weights
@@ -20,10 +18,13 @@ from pymilvus import Milvus, CollectionSchema, FieldSchema, DataType, Collection
 import base64
 from .models import Metadata
 from .serializers import MetadataSerializer
+from dotenv import load_dotenv
+
 load_dotenv()
 
 MILVUS_HOST = os.environ.get('MILVUS_HOST')
 MILVUS_PORT = os.environ.get('MILVUS_PORT')
+
 
 def image_to_base64(image_path):
     with open(image_path, 'rb') as f:
@@ -94,88 +95,48 @@ def get_image_embedding(image_path):
     return embedding
 
 
-def import_embeddings(image_directory, collection_name):
-    milvus = Milvus(host=MILVUS_HOST, port=MILVUS_PORT)
+@shared_task
+def process_image(image_path, collection_name):
     connections.connect(
         alias="default",
         host=MILVUS_HOST,
         port=MILVUS_PORT
     )
-    client = Milvus(MILVUS_HOST, MILVUS_PORT)
 
-    # Check if collection exists
-    if collection_name in milvus.list_collections():
-        collection = Collection(collection_name)
-        print(f"Collection '{collection_name}' already exists.")
+    collection = Collection(collection_name)
+    embedding = get_image_embedding(image_path)
+    base64_string = image_to_base64(image_path)
+    metadata = get_image_metadata(image_path)
+
+    # Generate UUID for the embedding
+    embedding_id = str(uuid.uuid4())
+    full_name = metadata['info']['FIO']
+
+    name_components = full_name.split()
+    first_name = None
+    surname = None
+    patronymic = None
+    # Check if all three components are present
+    if len(name_components) == 3:
+        surname, first_name, patronymic = name_components
+    elif len(name_components) == 2:
+        surname, first_name = name_components
+        patronymic = ""  # If patronymic is missing, assign an empty string
     else:
-        # Create collection with embedding and id fields
-        vector_id = FieldSchema(
-            name="vector_id",
-            dtype=DataType.VARCHAR,
-            is_primary=True,
-            max_length=36,
-            auto_id=False
-        )
-        vector = FieldSchema(
-            name="face_vector",
-            dtype=DataType.FLOAT_VECTOR,
-            dim=2048
-        )
-        schema = CollectionSchema(
-            fields=[vector_id, vector],
-            description="Collection of face embeddings",
-            enable_dynamic_field=True
-        )
-        collection = Collection(
-            name=collection_name,
-            schema=schema,
-            using='default'
-        )
+        print("Invalid full name format")
 
-    start = 1
-    # Import embeddings into Milvus
+    Metadata.objects.create(
+        vector_id=embedding_id,
+        firstName=first_name,
+        surname=surname,
+        patronymic=patronymic,
+        photo=base64_string
+    )
 
-    for index, file_name in enumerate(os.listdir(image_directory), start=start):
-        if file_name.endswith(".png") or file_name.endswith(".jpg"):
-            image_path = os.path.join(image_directory, file_name)
-            process_image.delay(image_path, collection_name)
+    data = [
+        [embedding_id],
+        [embedding]
+    ]
 
-    client.flush([collection_name])
-    stats = client.get_collection_stats(collection_name)
-    print(stats)
-
-    # Create index
-    index_params = {
-        "metric_type": "L2",
-        "index_type": "IVF_FLAT",
-        "params": {"nlist": 1024}
-    }
-    if collection:
-        collection.create_index(
-            field_name="face_vector",
-            index_params=index_params
-        )
-
-        utility.index_building_progress(collection_name)
-
-
-class MetadataViewSet(viewsets.ModelViewSet):
-    queryset = Metadata.objects.all()
-    serializer_class = MetadataSerializer
-    permission_classes = (IsAuthenticated,)
-
-    @action(detail=False, methods=['get'])
-    def commit(self, request, *args, **kwargs):
-        start_time = time.time()
-        image_directory = "C:/Users/User4/PycharmProjects/eTanuReincarnationAPI/metadata/data/images"
-
-        collection_name = 'face_embeddings'
-
-        import_embeddings(image_directory, collection_name)
-
-        end_time = time.time()
-        wasted_time = end_time - start_time
-
-        print("Request wasted time:", wasted_time)
-
-        return JsonResponse({'status': 'Success'})
+    collection.insert(data)
+    print(f"Done: {image_path}")
